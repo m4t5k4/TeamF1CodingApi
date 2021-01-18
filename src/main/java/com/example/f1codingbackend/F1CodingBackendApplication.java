3bc4b2cfc84dc6d76cd82f33662d9a46b1533e42
Matthias Reynders <matthias.reynders3@gmail.com>
2021-01-18 14:07:40 +0100

    init

---
+++ src/main/java/com/example/f1codingbackend/F1CodingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.f1codingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1CodingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1CodingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/f1codingbackend/F1CodingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.f1codingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1CodingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
