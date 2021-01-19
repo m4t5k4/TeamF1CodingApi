5b6c5aea0fff9dadcb17a6b8d6069f53893c541a
Ruben Vanderborght <r0749158@student.thomasmore.be>
2021-01-19 10:37:51 +0100

    relationships

--- src/main/java/com/example/f1codingbackend/model/TableLocation.java
+++ src/main/java/com/example/f1codingbackend/model/TableLocation.java
@@ -1,9 +1,8 @@
 package com.example.f1codingbackend.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class TableLocation {
@@ -38,5 +37,38 @@ public class TableLocation {
 
     public void setLocationId(int locationId) {
         this.locationId = locationId;
+    }
+
+    @OneToMany
+    private List<Reservation> reservations = new ArrayList<>();
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    @OneToMany
+    private List<Place> places = new ArrayList<>();
+
+    public List<Place> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(List<Place> places) {
+        this.places = places;
+    }
+
+    @ManyToOne
+    private Location location;
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
     }
 }
