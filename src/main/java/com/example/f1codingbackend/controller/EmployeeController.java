4714bca57c6cfa7d8727f0134dbd228139dc592c
Ruben Vanderborght <r0749158@student.thomasmore.be>
2021-01-19 11:45:59 +0100

    first get employee

--- src/main/java/com/example/f1codingbackend/controller/EmployeeController.java
+++ src/main/java/com/example/f1codingbackend/controller/EmployeeController.java
@@ -1,17 +1,19 @@
 package com.example.f1codingbackend.controller;
-
 import com.example.f1codingbackend.model.Employee;
 import com.example.f1codingbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @RestController
 public class EmployeeController {
     @Autowired
     EmployeeRepository employeeRepository;
+
     @PostConstruct
     public void render()
     {
@@ -25,5 +27,12 @@ public class EmployeeController {
             employee.setRoleId(1);
             employeeRepository.save(employee);
         }
+    }
+
+    @GetMapping("/employee")
+    public List<Employee> employees()
+    {
+        List<Employee> employeeList = employeeRepository.findAll();
+        return  employeeList;
     }
 }
