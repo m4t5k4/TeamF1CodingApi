981c671970370c3bb179f02d48b9be9ea2b27c3c
JensWeyen <r0751341@student.thomasmore.be>
2021-01-20 14:46:23 +0100

    script place generation and adding them to the tables

--- src/main/java/com/example/f1codingbackend/controller/MainController.java
+++ src/main/java/com/example/f1codingbackend/controller/MainController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 @RestController
 public class MainController {
@@ -130,13 +132,29 @@ public class MainController {
             }
         }
         if (placeRepository.count()==0){
-            String[] names = {"place 1","place 2","place 3","place 4"};
-            for (String name : names) {
-                int bound = ((int) tableRepository.count());
-                int rand = new Random().nextInt(bound-1)+1;
+            for (int i = 1; i<=240;i++) {
                 Place newplace = new Place();
-                newplace.setTableLocation(tableRepository.findById(rand));
                 placeRepository.save(newplace);
+            }
+            Integer[] placeNums = {8,16,24,30,34,38,42,52,62,68,74,80,86,92,98,104,110,116,122,126,136,146,
+                    154,162,170,178,182,186,190,194,202,210,216,224,232,238,240};
+            ArrayList<Integer> values = new ArrayList<>();
+            for (int id: placeNums) {
+                values.add(id);
+            }
+
+            for (int num:values) {
+                int start = 0;
+                if (values.indexOf(num)!=0)
+                    start = values.get(values.indexOf(num)-1);
+                IntStream.range(start, num).forEachOrdered(n ->
+                {
+                    Place addPlaceToTable = placeRepository.findById(n+1);
+                    System.out.println(values.indexOf(num));
+                    addPlaceToTable.setTableLocation(tableRepository.findById(values.indexOf(num)+1));
+                    placeRepository.save(addPlaceToTable);
+                });
+
             }
         }
         if (employeeRepository.count()==0)
